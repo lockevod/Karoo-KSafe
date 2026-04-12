dc910756620fb8d78b400e67188bf05d69a32b13
Enderthor <58392928+lockevod@users.noreply.github.com>
2026-04-12 15:39:12 +0200

    Improve Readme.

--- app/src/main/kotlin/com/enderthor/kSafe/KSafeApplication.kt
+++ app/src/main/kotlin/com/enderthor/kSafe/KSafeApplication.kt
@@ -1,6 +1,7 @@
 package com.enderthor.kSafe
 
 import android.app.Application
+import android.util.Log
 import timber.log.Timber
 
 class KSafeApplication : Application() {
@@ -8,6 +9,17 @@ class KSafeApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        } else {
+            // In release: only WARN and ERROR reach logcat (useful for ADB diagnostics)
+            // DEBUG/INFO/VERBOSE are stripped entirely by ProGuard -assumenosideeffects
+            Timber.plant(object : Timber.Tree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    if (priority >= Log.WARN) {
+                        Log.println(priority, tag ?: "KSafe", message)
+                        t?.let { Log.e(tag ?: "KSafe", message, it) }
+                    }
+                }
+            })
         }
     }
 }
